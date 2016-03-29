fafccd10e82337768008100efbc63568fd267969
bojpop <poppfc@gmail.com>
2016-03-30 01:19:55 +0200

    First commit

---
+++ Calculator/FileLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class FileLogger : Calculator
+    {
+        private Calculator calc = new Calculator();
+            int Add(int a, int b)
+            {
+                Console.WriteLine("Logging in file");
+                return calc.Add(a,b);
+            }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileLogger fileLogger = new FileLogger();
+
+            var sum = fileLogger.Add(5, 6);
+            Console.WriteLine("Sum of a & b is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
