a0f7919408d1f0bbaabd43d4dbf6efb26292cc6c
Bojan M Popović <bojan.m.popovic@posta.jpptt.internal>
2016-04-28 14:06:24 +0200

    no message

--- Calculator/ConsoleLogger.cs
+++ Calculator/ConsoleLogger.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class ConsoleLogger
+    class ConsoleLogger : Calculator
     {
         private Calculator calc = new Calculator();
         override public int Add(int a, int b)
         {
             Console.WriteLine("Logging in console");
-            return calc.Add(a, b);
+            int res = base.Add(5, 6);
+            Console.WriteLine("Result is: ", res);
+            return res;
         }
     }
 }
